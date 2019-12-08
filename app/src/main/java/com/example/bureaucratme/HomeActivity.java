8c36703915abadd3f06b83931897e4693b939aa6
unknown <lironarad1@gmail.com>
2019-12-08 14:45:31 +0200

    add menue button, personal details activity and connection to firebase database

---
+++ app/src/main/java/com/example/bureaucratme/HomeActivity.java
@@ -0,0 +1,74 @@
+package com.example.bureaucratme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    FirebaseUser fu;
+    FirebaseAuth mAuth;
+    TextView tv;
+    Button btnSignOut;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnSignOut = findViewById(R.id.btnSignOut);
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                Toast.makeText(HomeActivity.this, "Sign out", Toast.LENGTH_SHORT).show();
+                signOut();
+            }
+        });
+
+        tv = findViewById(R.id.textView);
+        fu = mAuth.getCurrentUser();
+        if(fu != null) {
+            tv.setText(fu.getEmail());
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent myIntent = new Intent(HomeActivity.this, PersonalDetailsActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(myIntent);
+        finish();
+        return true;
+    }
+
+
+    private void signOut() {
+        Intent myIntent = new Intent(this, MainActivity.class);
+        myIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(myIntent);
+        finish();
+
+    }
+}
